df01b76323a42e24c1843741d0914b4a4f0ca815
erue24 <92362388+erue24@users.noreply.github.com>
2022-03-16 15:49:07 +0000

    Added dummy data, created two sessions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,95 @@
 
 import './App.css';
 
+
+
+
+// Creating an array for all the dummy Data aka the names 
+//Create all the elements as JS objects 
+const defualtClassList = [
+  {
+    id: 1,
+    fName: "Angelica",
+    present: false
+  },
+  {
+    id: 2,
+    fName: "John",
+    present: false
+  },
+  {
+    id: 3,
+    fName: "Gabe",
+    present: false
+  },
+  {
+    id: 4,
+    fName: "Emilie",
+    present: false
+  }
+];
+
+// Creating an array for all the dummy Data. This data is sessions that has a name, date, start and end time 
+//Create all the elements as JS objects 
+const defaultSession = [
+  {
+    id: 1, name: "WDF Practical",
+    date: "02/01/2022",
+    sTime: "14:00",
+    eTime: "15:00",
+    rollcall: [
+      {
+        id: 1,
+        fName: "Angelica",
+        present: false
+      },
+      {
+        id: 2,
+        fName: "John",
+        present: false
+      },
+      {
+        id: 3,
+        fName: "Gabe",
+        present: false
+      },
+      {
+        id: 4,
+        fName: "Emilie",
+        present: false
+      }
+    ]
+  },
+  {
+    id: 2, name: "Design 101",
+    date: "05/02/2022",
+    sTime: "10:00",
+    eTime: "12:00",
+    rollcall: [
+      {
+        id: 1,
+        fName: "Angelica",
+        present: false
+      },
+      {
+        id: 2,
+        fName: "John",
+        present: false
+      },
+      {
+        id: 3,
+        fName: "Gabe",
+        present: false
+      },
+      {
+        id: 4,
+        fName: "Emilie",
+        present: false
+      }
+    ]
+  }
+];
+
 function App() {
   return (
     <div className="App">
